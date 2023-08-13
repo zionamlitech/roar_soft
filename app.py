a0a0ce5e4fb8b0ca6022953bdc806f3d1c76a7c2
zionamalitech <isaac.okyere@amalitech.org>
2023-08-13 11:45:19 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+Company=[
+    {
+        'id':1,'project':'web development','status':'available'
+    },
+     {
+        'id':2,'project':'form development','status':'available'
+    },
+     {
+        'id':3,'project':'Database Management','status':'available'
+    }
+]
+app = Flask(__name__)
+
+@app.route("/")
+def hello_isaac():
+    return render_template('index.html', company=Company)
+
+@app.route("/header.html")
+def head():
+    return render_template('header.html')
+if __name__ =='__main__':
+    app.run(host='0.0.0.0', debug=True)
